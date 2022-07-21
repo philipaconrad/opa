6c03d7ca3f76128b7ea5f001f4063cf30bae8982
Philip Conrad <philipaconrad@gmail.com>
2022-07-21 17:13:03 -0400

    topdown/cache: Add NDBuiltinResultCache and implementation.
    Signed-off-by: Philip Conrad <philipaconrad@gmail.com>

--- topdown/cache/cache.go
+++ topdown/cache/cache.go
@@ -165,3 +165,89 @@ func (c *cache) maxSizeBytes() int64 {
 	}
 	return *c.config.InterQueryBuiltinCache.MaxSizeBytes
 }
+
+// Note(philipc): The non-deterministic builtins cache exists to allow
+// decision logging to preserve the results of different non-deterministic
+// builtin function calls. This paves the way for later reconstruction and
+// replay of policies from their decision logs.
+// There are constraints however, namely that we only save the first
+// result we see for a given set of input parameters. This means that for
+// builtins like http.send, we assume that the outside world is able to
+// return arbitrary content, but that the result will be idempotent across
+// successive calls of that builtin with the same inputs. Otherwise, we
+// begin to descend into selective tracing of just the non-deterministic
+// builtins.
+type NDConfig interface {
+}
+
+// NDBuiltinResultCache defines the interface for the specialized cache.
+type NDBuiltinResultCache interface {
+	Get(builtinName string, k *ast.Term) (value *ast.Term, found bool)
+	Insert(builtinName string, k *ast.Term, value *ast.Term)
+	Delete(builtinName string, k *ast.Term)
+	//UpdateConfig(config *NDConfig)
+}
+
+// Two-level map:
+// - First layer: builtin name
+//   - Second layer: inputs -> outputs
+// Note(philipc): This is mostly a ripoff of the inter-query cache.
+// We suffer here because we stringify the keys to simplify insertion.
+type ndBuiltinResultCache struct {
+	items  map[string]map[string]*ast.Term
+	config *NDConfig
+	mtx    sync.Mutex
+}
+
+// NewInterQueryCache returns a new inter-query cache.
+func NewNDBuiltinResultCache(config *NDConfig) NDBuiltinResultCache {
+	return &ndBuiltinResultCache{
+		items:  map[string]map[string]*ast.Term{},
+		config: config,
+	}
+}
+
+// Insert inserts a key k into the cache with value v.
+func (c *ndBuiltinResultCache) Insert(builtinName string, k *ast.Term, v *ast.Term) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	entries, exists := c.items[builtinName]
+	if !exists {
+		c.items[builtinName] = make(map[string]*ast.Term)
+	}
+	entries[k.String()] = v // k will be an array term.
+}
+
+// Get returns the value in the cache for k.
+func (c *ndBuiltinResultCache) Get(builtinName string, k *ast.Term) (*ast.Term, bool) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	// If the second-level map doesn't exist, exit early.
+	if _, exists := c.items[builtinName]; !exists {
+		return nil, false
+	}
+	// If the entry doesn't exist, exit early. Otherwise return the value.
+	entry, exists := c.items[builtinName][k.String()]
+	if !exists {
+		return nil, false
+	}
+	return entry, true
+}
+
+func (c *ndBuiltinResultCache) Delete(builtinName string, k *ast.Term) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	// Delete the value if its second-level map exists.
+	if _, exists := c.items[builtinName]; exists {
+		delete(c.items[builtinName], k.String())
+	}
+}
+
+// func (c *ndBuiltinResultCache) UpdateConfig(config *NDConfig) {
+// 	if config == nil {
+// 		return
+// 	}
+// 	c.mtx.Lock()
+// 	defer c.mtx.Unlock()
+// 	c.config = config
+// }
